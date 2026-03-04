013222cdda7fcb319ef91569ce8ee4a4cdba90fa
luawutt <viktoriakviieti@gmail.com>
2026-03-04 01:41:43 +0100

    Updated bank simulator

--- Bank Account Simulator/Program.cs
+++ Bank Account Simulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata;
 namespace BankSimulator
 {
     class Program
@@ -6,48 +7,113 @@ namespace BankSimulator
         static void Main(string[] args)
         {
             double balance = 1000;
+            string pin = "1234";
+            string username = "student";
             bool running = true;
+            int attempts = 0;
+            string history = "";
 
-            while (running)
+
+            //while part 
+            while (attempts < 0)
             {
-                Console.WriteLine("===== BANK MENU =====");
-                Console.WriteLine("1 - Check balance ");
-                Console.WriteLine("2 - Deposit ");
-                Console.WriteLine("3 - WithDraw ");
-                Console.WriteLine("4 - Exit ");
+                Console.WriteLine("Write your login: ");
+                string usercheck = Console.ReadLine();
+                Console.WriteLine("Write your password: ");
+                string pincheck = Console.ReadLine();
 
-                Console.WriteLine("Choose option: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
-
-                //if part 
-                if (choice == 1)
+                //if part
+                if (username == usercheck && pin == pincheck)
                 {
-                    Console.WriteLine("Balance: " + balance);
+                    break;
                 }
 
-                else if (choice == 2)
+                else
                 {
-                    Console.WriteLine("Enter amount to deposit: ");
-                    double deposit = Convert.ToDouble(Console.ReadLine());
-                    balance = balance + deposit;
+                    attempts++;
                 }
 
-                else if (choice == 3)
+                //second while
+                while (running)
                 {
-                    Console.WriteLine("Enter amount to withdraw: ");
-                    double withdraw = Convert.ToDouble(Console.ReadLine());
-                    if (withdraw > balance)
+                    //menu console
+                    Console.WriteLine("==== MENU ====");
+                    Console.WriteLine("1 - Check balance ");
+                    Console.WriteLine("2 - Deposit ");
+                    Console.WriteLine("3 - Withdraw ");
+                    Console.WriteLine("4 - Transaction history ");
+                    Console.WriteLine("5 - Exit");
+                    Console.WriteLine("Choose option: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+
+                    // balance
+                    if (choice == 1)
                     {
-                        Console.WriteLine("Not enough funds");
+                        Console.WriteLine("Balance: " + balance);
                     }
-                    else
+
+
+                    // deposit
+                    if (choice == 2)
                     {
-                        balance = balance - withdraw;
+                        Console.WriteLine("Amount to deposit: ");
+
+                        double deposit = Convert.ToDouble(Console.ReadLine());
+                        if (deposit <= 0)
+                        {
+                            Console.WriteLine("Invalid amount");
+                        }
+                        else
+                        {
+                            balance = balance + deposit;
+                            Console.WriteLine("Operation successful");
+                        }
+                    }
+
+
+                    // withdraw
+                    if (choice == 3)
+                    {
+                        Console.WriteLine("Amount to withdraw: ");
+                        int widthdraw = Convert.ToInt32(Console.ReadLine());
+                        if ((widthdraw < 0) || (widthdraw > balance))
+                        {
+                            Console.WriteLine("Invalid amount");
+                        }
+
+                        else
+                        {
+                            balance = balance - widthdraw;
+                            Console.WriteLine("Operation successful");
+                        }
+
+
+                        // transaction history
+                        history += "Deposited= " + deposit  + "\n";
+                        history += "Widthrawed: " + widthdraw + "\n";
+
+                        //exit
+                        if (choice == 4)
+                        {
+                            Console.WriteLine("Are you sure (yes/no): ");
+                            string exitcheck = Console.ReadLine();
+                            if (exitcheck == "yes")
+                            {
+                                running = false;
+                                Console.WriteLine("Goodbye!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Returning to menu...");
+                            }
+                        }
 
                     }
                 }
+
+
             }
         }
     }
-
 }
